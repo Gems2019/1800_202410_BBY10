b31f055cab762e19a6de150b4fe77857d13ff59f
AoceanP <unawakened.luck@gmail.com>
2024-03-06 14:18:40 -0800

    Unit

--- scripts/script1.js
+++ scripts/script1.js
@@ -56,3 +56,38 @@ function writeunit() {
         last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
     });
 }
+
+
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeLoop(max) {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+    for (i = 1; i <= max; i++) {
+        hikesRef.add({ //add to database, autogen ID
+            name: "hike" + i,
+            details: "Elmo says this hike is amazing!  You will love going on hike" + i,
+            lat: 49+i,    //randomly different
+            lng: -122+i,  //randomly different
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        })
+   }
+}
+
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeunitLoop(max) {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("AvailableUnit");
+    for (i = 1; i <= max; i++) {
+        hikesRef.add({ //add to database, autogen ID
+            name: "unit" + i,
+            details: "Bedroom/Bathroom:" + "  " 
+                + "Location:" + i,
+            code: unit1,    
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        })
+   }
+}
