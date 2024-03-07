b6bfe0852008bb1c76b08dded093d386111d7930
Gem <bsha@my.bcit.ca>
2024-03-07 10:11:05 -0800

    update

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,29 +2,41 @@
 // This function loads the parts of your skeleton
 // (navbar, footer, and other things) into html doc.
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
-}
-loadSkeleton();  //invoke the function
-
-//---------------------------------------------------
-// This function loads the parts of your skeleton
-// (navbar, footer, and other things) into html doc.
-//---------------------------------------------------
 function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
-            console.log($('#navbarPlaceholder').load('/main.html'));
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
             // No user is signed in.
-            console.log($('#navbarPlaceholder').load('/welcome.html'));
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         }
     });
 }
 loadSkeleton(); //invoke the function
+
+//---------------------------------
+// Your own functions here
+//---------------------------------
+
+
+function sayHello() {
+    //do something
+}
+//sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        console.log("error: ", error)
+      });
+}
