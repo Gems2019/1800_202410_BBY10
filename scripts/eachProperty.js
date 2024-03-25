19d25b723a80fcc49e9396024bb48703a1798247
Gem <bsha@my.bcit.ca>
2024-03-24 23:22:01 -0700

    update

--- scripts/eachProperty.js
+++ scripts/eachProperty.js
@@ -1,6 +1,89 @@
-function saveHikeDocumentIDAndRedirect(){
+function displayUnitInfo() {
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    console.log( ID );
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection( "rentals" )
+        .doc( ID )
+        .get()
+        .then( doc => {
+            thisUnit = doc.data();
+            unitCode = thisUnit.code;
+            unitName = doc.data().name;
+            
+            // only populate title, and image
+            document.getElementById( "unitName" ).innerHTML = unitName;
+            let imgEvent = document.querySelector( ".unit-img" );
+            imgEvent.src = "../images/" + unitCode + ".jpg";
+        } );
+}
+displayUnitInfo();
+
+function saveRentDocumentIDAndRedirect(){
     let params = new URL(window.location.href) //get the url from the search bar
     let ID = params.searchParams.get("docID");
-    localStorage.setItem('hikeDocID', ID);
-    window.location.href = 'review.html';
+    localStorage.setItem('renterDocID', ID);
+    window.location.href = 'renterInfo.html';
 }
+
+function populateRenters() {
+    console.log("test");
+    let hikeCardTemplate = document.getElementById("renterCardTemplate");
+    let hikeCardGroup = document.getElementById("renterCardGroup");
+
+    let params = new URL(window.location.href); // Get the URL from the search bar
+    let hikeID = params.searchParams.get("docID");
+
+    // Double-check: is your collection called "Reviews" or "reviews"?
+    db.collection("")
+        .where("renterDocID", "==", renterID)
+        .get()
+        .then((allRenters) => {
+            renters = allRenters.docs;
+            console.log(renters);
+            reviews.forEach((doc) => {
+                var title = doc.data().title;
+                var level = doc.data().level;
+                var season = doc.data().season;
+                var description = doc.data().description;
+                var pet = doc.data().pet;
+                var alone = doc.data().alone;
+                var time = doc.data().timestamp.toDate();
+                var rating = doc.data().rating; // Get the rating value
+                console.log(rating)
+
+                console.log(time);
+
+                let reviewCard = hikeCardTemplate.content.cloneNode(true);
+                reviewCard.querySelector(".title").innerHTML = title;
+                reviewCard.querySelector(".time").innerHTML = new Date(
+                    time
+                ).toLocaleString();
+                reviewCard.querySelector(".level").innerHTML = `Level: ${level}`;
+                reviewCard.querySelector(".season").innerHTML = `Season: ${season}`;
+                reviewCard.querySelector(".alone").innerHTML = `Alone: ${alone}`;
+                reviewCard.querySelector(".pet").innerHTML = `Pet: ${pet}`;
+                reviewCard.querySelector( ".description").innerHTML = `Description: ${description}`;
+
+                // Populate the star rating based on the rating value
+                
+	              // Initialize an empty string to store the star rating HTML
+								let starRating = "";
+								// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+                for (let i = 0; i < rating; i++) {
+                    starRating += '<span class="material-icons">star</span>';
+                }
+								// After the first loop, this second loop runs from i=rating to i<5.
+                for (let i = rating; i < 5; i++) {
+                    starRating += '<span class="material-icons">star_outline</span>';
+                }
+                reviewCard.querySelector(".star-rating").innerHTML = starRating;
+
+                hikeCardGroup.appendChild(reviewCard);
+            });
+        });
+}
+
+populateReviews();
+
---
+++ scripts/renterInfo.js
@@ -0,0 +1,63 @@
+var hikeDocID = localStorage.getItem("unitDocID");    //visible to all functions on this page
+function getUnitName(id) {
+    db.collection("units")
+      .doc(id)
+      .get()
+      .then((thisHike) => {
+        var unitName = thisUnit.data().name;
+        document.getElementById("unitName").innerHTML = unitName;
+          });
+}
+
+getUnitName(unitDocID);
+
+function writeReview() {
+    console.log("inside write review");
+    let unitTitle = document.getElementById("title").value;
+    let unitLevel = document.getElementById("level").value;
+    let unitSeason = document.getElementById("season").value;
+    let unitDescription = document.getElementById("description").value;
+    let unitPet = document.querySelector('input[name="pet"]:checked').value;
+    let unitAlone = document.querySelector('input[name="alone"]:checked').value;
+
+    // Get the star rating
+		// Get all the elements with the class "star" and store them in the 'stars' variable
+    const stars = document.querySelectorAll('.star');
+		// Initialize a variable 'hikeRating' to keep track of the rating count
+    let hikeRating = 0;
+		// Iterate through each element in the 'stars' NodeList using the forEach method
+    stars.forEach((star) => {
+				// Check if the text content of the current 'star' element is equal to the string 'star'
+        if (star.textContent === 'star') {
+						// If the condition is met, increment the 'hikeRating' by 1
+            hikeRating++;
+        }
+    });
+
+    console.log(unitTitle, unitLevel, unitSeason, unitDescription, unitPet, unitAlone, hikeRating);
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+        var userID = user.uid;
+
+        // Get the document for the current user.
+        db.collection("reviews").add({
+            hikeDocID: hikeDocID,
+            userID: userID,
+            title: hikeTitle,
+            level: hikeLevel,
+            season: hikeSeason,
+            description: hikeDescription,
+            pet: hikeFlooded,
+            alone: hikeScrambled,
+            rating: hikeRating, // Include the rating in the review
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            window.location.href = "thanks.html"; // Redirect to the thanks page
+        });
+    } else {
+        console.log("No user is signed in");
+        window.location.href = 'renterInfo.html';
+    }
+}
