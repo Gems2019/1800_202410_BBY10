7c5fe94745aaf56fd096e9d2c11cf837756f2b63
Gem <bsha@my.bcit.ca>
2024-03-24 20:52:58 -0700

    update eachproperty

---
+++ scripts/eachProperty.js
@@ -0,0 +1,6 @@
+function saveHikeDocumentIDAndRedirect(){
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
