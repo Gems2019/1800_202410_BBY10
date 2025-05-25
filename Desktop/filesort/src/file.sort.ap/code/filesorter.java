079d56861065c9516e8365e2fb7d1b8c77863537
AoceanP <unawakened.luck@gmail.com>
2025-05-25 03:09:41 -0700

    Initial push

---
+++ Desktop/filesort/src/file.sort.ap/code/filesorter.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+/**
+ * My files are unorganized, I want a better way to manage them. So I created a file sorter
+ * to assist a solution. The class will scan a folder and list all the files in it, then sort
+ * the files into the respective folders.
+ *
+ * @author Aleksandar Panich
+ * @version 1.0
+ * */
+public class filesorter
+{
+    public static void main(String[] args) {
+
+    }
+}
